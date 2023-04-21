f3dfe2a1495affb41b2029d9c6115853b1272e30
Sarman-Dasa <sarmand@zignuts.com>
2023-04-21 19:14:22 +0530

    add lable tag and hide show input filed

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -2,9 +2,27 @@
     <h2>
         <table id="usertable">
             <h3>User Data</h3>
-            Name : <input type="text" placeholder="Enter name" v-model="name">
-            Email : <input type="email" placeholder="Enter email" v-model="email">
-            Password : <input type="password" placeholder="Enter password" v-model="password">
+            
+            <label for="">Name</label>
+            <input type="text" placeholder="Enter name" v-model="name">
+            
+            <label for="">email</label>
+            <input type="email" placeholder="Enter email" v-model="email">
+            <br>
+            
+            <label for="">Pasword</label>
+            <input type="password" placeholder="Enter password" v-model="password">
+
+            <label for="">phone</label>
+            <input type="text" placeholder="Enter mobile number">
+            <br>
+
+            <label for="snumber" style="font-size: 12px;">2nd number</label>
+            <input type="checkbox" id="snumber" v-on:click="snumber=!snumber">
+            <br>
+            <label v-if="snumber" for="">phone</label>
+            <input v-if="snumber" type="text" placeholder="Enter mobile number">
+
             <br>
             <br>
             <!-- User skill checkbox -->
@@ -52,13 +70,13 @@ export default {
             output: null,
             skills: [],
             gender: null,
+            snumber:false,
         }
     },
     methods: {
         getData() {
             this.output = this.name + " " + this.email + " " + this.password +" Skills :"+ this.skills + " Gender :" + this.gender;
-
-        }
+        },
     }
 }
 </script>
@@ -68,6 +86,8 @@ h1 {
     color: white;
 }
 label {
-    padding-left: 10px;
+   margin: 0px 20px;
+   font-size: 14px;
 }
+
 </style>
