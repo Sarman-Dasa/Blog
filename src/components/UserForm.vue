e7958f0238bd6114208881c570748f71a34d18b7
Sarman-Dasa <sarmand@zignuts.com>
2023-04-21 17:56:49 +0530

    get a checkbox and radio button value

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -6,8 +6,36 @@
             Email : <input type="email" placeholder="Enter email" v-model="email">
             Password : <input type="password" placeholder="Enter password" v-model="password">
             <br>
-            <button v-on:click="getData()">Get Data</button>
             <br>
+            <!-- User skill checkbox -->
+            <label for="">Select Skill :</label>
+            <!-- php checkbox -->
+            <label for="php">Php</label>
+            <input type="checkbox" id="php" value="php" v-model="skills">
+        
+            <!-- laravel checkbox -->
+            <label for="laravel">Laravel</label>
+            <input type="checkbox" id="laravel" value="laravel" v-model="skills">
+        
+            <!-- Vue js -->
+            <label for="vuejs">Vue js</label>
+            <input type="checkbox" id="vuejs" value="vuejs" v-model="skills">
+            <br>
+
+            <!--Radio Button select gender -->
+            <label for="">Select your gender :</label>
+
+            <!-- male radio button -->
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="male" value="male" v-model="gender">
+
+            <!-- female radio button -->
+            <label for="female">Female</label>
+            <input type="radio" name="gender" id="female" value="female" v-model="gender">
+
+            <br>
+            <button v-on:click="getData()">Get Data</button>
+
             <p>{{ output }}</p>
         </table>
     </h2>
@@ -22,11 +50,14 @@ export default {
             email: null,
             password: null,
             output: null,
+            skills: [],
+            gender: null,
         }
     },
     methods: {
         getData() {
-            this.output = this.name + " " + this.email + " " + this.password;
+            this.output = this.name + " " + this.email + " " + this.password +" Skills :"+ this.skills + " Gender :" + this.gender;
+
         }
     }
 }
@@ -35,5 +66,8 @@ export default {
 <style scoped>
 h1 {
     color: white;
+}
+label {
+    padding-left: 10px;
 }
 </style>
