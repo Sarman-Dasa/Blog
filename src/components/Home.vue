6bff331a135303d3cf2c5091ebbe7edc09c741fd
Sarman-Dasa <sarmand@zignuts.com>
2023-04-21 12:49:16 +0530

    create a home component

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Home component</h1>
+</template>
+
+<script>
+    export default {
+        name:'HomeView',
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: rgb(17, 187, 187);
+    }
+</style>
