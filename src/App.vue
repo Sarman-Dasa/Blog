aaf3e005e4b594aa5cf3bbb7fdcf54521523c173
Sarman-Dasa <sarmand@zignuts.com>
2023-04-21 17:05:38 +0530

    comment in userform component code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
   <Home />
+  <!-- <UserForm /> -->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/Home.vue';
+import Home from './components/Home.vue'
+// import UserForm from './components/UserForm.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Home,
-}
+    // UserForm,
+  }
 }
 </script>
 
