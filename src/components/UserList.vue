d539bfc8c96075470d34cd1add40777f9bdf17c6
Sarman-Dasa <sarmand@zignuts.com>
2023-04-25 12:44:57 +0530

    print a userlist

---
+++ src/components/UserList.vue
@@ -0,0 +1,19 @@
+<template>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.phone }}</td>
+            <td>{{ user.gender }}</td>
+            <td>
+                <button v-on:click="userId(user.id)">User Id</button>
+            </td>
+</template>
+
+<script>
+    export default {
+        name: "UserLisr",
+        props: {
+            user: Object,
+            userId: Function,
+        }
+    }
+</script>
