6985ef37b4fddb2318c76b8396926c2828aa9bac
Sarman-Dasa <sarmand@zignuts.com>
2023-04-25 19:44:51 +0530

    add watcher to check a volume level

---
+++ src/components/Watcher.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Volume Tracker (0-20)</h1>
+    <h2>Current Volume :{{ volume }}</h2>
+    <input type="range" v-model="volume" min="0" max="20">
+</template>
+
+<script>
+    export default {
+        name:"watchersView",
+        data() {
+            return {
+                volume: 0,
+            }
+        },
+        watch: {
+            volume(newValue, oldValue) {
+                //console.log("New Value:" + newValue + " oldValue: "+oldValue);
+                if(newValue > oldValue && newValue==15) {
+                    alert("Volume is high to damage your ears!!!");
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+*{
+    text-align: center;
+}
+</style>
