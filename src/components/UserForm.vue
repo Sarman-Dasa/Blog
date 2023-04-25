ae84f409457d5575ab0e576ace47013023607243
Sarman-Dasa <sarmand@zignuts.com>
2023-04-25 16:03:55 +0530

    use ref in form

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,60 +1,61 @@
 <template>
-        <table id="usertable">
-            <h3>User Data</h3>
-            
-            <label for="">Name</label>
-            <input type="text" placeholder="Enter name" v-model="name">
-            
-            <label for="">email</label>
-            <input type="email" placeholder="Enter email" v-model="email">
-            <br>
-            
-            <label for="">Pasword</label>
-            <input type="password" placeholder="Enter password" v-model="password">
+    <table id="usertable">
+        <h3>User Data</h3>
 
-            <label for="">phone</label>
-            <input type="text" placeholder="Enter mobile number">
-            <br>
+        <label for="">Name</label>
+        <input type="text" placeholder="Enter name" v-model="name" ref="name">
 
-            <label for="snumber" style="font-size: 12px;">2nd number</label>
-            <input type="checkbox" id="snumber" v-on:click="snumber=!snumber">
-            <br>
-            <label v-if="snumber" for="">phone</label>
-            <input v-if="snumber" type="text" placeholder="Enter mobile number">
+        <label for="">email</label>
+        <input type="email" placeholder="Enter email" v-model="email">
+        <br>
 
-            <br>
-            <br>
-            <!-- User skill checkbox -->
-            <label for="">Select Skill :</label>
-            <!-- php checkbox -->
-            <label for="php">Php</label>
-            <input type="checkbox" id="php" value="php" v-model="skills">
-        
-            <!-- laravel checkbox -->
-            <label for="laravel">Laravel</label>
-            <input type="checkbox" id="laravel" value="laravel" v-model="skills">
-        
-            <!-- Vue js -->
-            <label for="vuejs">Vue js</label>
-            <input type="checkbox" id="vuejs" value="vuejs" v-model="skills">
-            <br>
+        <label for="">Pasword</label>
+        <input type="password" placeholder="Enter password" v-model="password">
 
-            <!--Radio Button select gender -->
-            <label for="">Select your gender :</label>
+        <label for="">phone</label>
+        <input type="text" placeholder="Enter mobile number" v-model="phone">
+        <br>
 
-            <!-- male radio button -->
-            <label for="male">Male</label>
-            <input type="radio" name="gender" id="male" value="male" v-model="gender">
+        <label for="snumber" style="font-size: 12px;">2nd number</label>
+        <input type="checkbox" id="snumber" v-on:click="snumber = !snumber">
+        <br>
+        <label v-if="snumber" for="">phone</label>
+        <input v-if="snumber" type="text" placeholder="Enter mobile number">
 
-            <!-- female radio button -->
-            <label for="female">Female</label>
-            <input type="radio" name="gender" id="female" value="female" v-model="gender">
+        <br>
+        <br>
+        <!-- User skill checkbox -->
+        <label for="">Select Skill :</label>
+        <!-- php checkbox -->
+        <label for="php">Php</label>
+        <input type="checkbox" id="php" value="php" v-model="skills">
 
-            <br>
-            <button v-on:click="getData()">Get Data</button>
+        <!-- laravel checkbox -->
+        <label for="laravel">Laravel</label>
+        <input type="checkbox" id="laravel" value="laravel" v-model="skills">
 
-            <p>{{ output }}</p>
-        </table>
+        <!-- Vue js -->
+        <label for="vuejs">Vue js</label>
+        <input type="checkbox" id="vuejs" value="vuejs" v-model="skills">
+        <br>
+
+        <!--Radio Button select gender -->
+        <label for="">Select your gender :</label>
+
+        <!-- male radio button -->
+        <label for="male">Male</label>
+        <input type="radio" name="gender" id="male" value="male" v-model="gender">
+
+        <!-- female radio button -->
+        <label for="female">Female</label>
+        <input type="radio" name="gender" id="female" value="female" v-model="gender">
+
+        <br>
+        <button v-on:click="getData()">Get Data</button>
+        <button v-on:click="clearData()">Clear</button>
+
+        <p>{{ output }}</p>
+    </table>
 </template>
 
 <script>
@@ -68,13 +69,20 @@ export default {
             output: null,
             skills: [],
             gender: null,
-            snumber:false,
+            snumber: false,
+            phone: null,
         }
     },
     methods: {
         getData() {
-            this.output = this.name + " " + this.email + " " + this.password +" Skills :"+ this.skills + " Gender :" + this.gender;
+            this.output = this.name + " " + this.email + " " + this.password + " Skills :" + this.skills + " Gender :" + this.gender;
         },
+        clearData() {
+            this.output = this.name = this.email = this.password = this.skills = this.gender = this.phone = "";
+            let name = this.$refs.name;
+            name.focus();
+            name.style.color = "green";
+        }
     }
 }
 </script>
@@ -83,9 +91,9 @@ export default {
 h1 {
     color: white;
 }
-label {
-   margin: 0px 20px;
-   font-size: 14px;
-}
 
+label {
+    margin: 0px 20px;
+    font-size: 14px;
+}
 </style>
