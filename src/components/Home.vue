e32612c7e607319268889fb64a8cf9cbd683b23b
Sarman-Dasa <sarmand@zignuts.com>
2023-04-25 11:33:13 +0530

    pass data into child component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,49 +1,68 @@
 <template>
-<div id="left">
-    <h1 v-on:mousemove="getConsole()">Home component</h1>
-    <p>Employee FirstName : {{ firstName }}</p>
-    <p>Employee LastName : {{ lastName }}</p>
-    <p>Email : {{ email }}</p>
-    <p>Mobile Number : {{ mobile }}</p>
-    <!-- print a full name  -->
-    <p>Full Name : {{ getFullName() }}</p>
-    <!-- print all data -->
-    <p>Get All Detail :{{ getDeta() }}</p>
-    <!-- print a only first name into data -->
-    <p>Name : {{ getDeta().firstName }}</p>
-    <p>Total Click : {{ count }}</p>
-    <button v-on:click="countClicks()">Click!!!</button>
-    <button v-on:dblclick="getMobilenumber()" >Double Click</button>
-</div>
-<div id="rigth">
-    <h2>Second Div</h2>
-    <input type="number" v-model="count">
-    <input type="text" v-model="name">
-    <p>Your name is : {{ name }}</p>
-    <UserForm />
-</div>
-<div id="clearboth">
-<div style="display:flex">
-    <ListData />
-</div>
-</div>
+    <div id="left">
+        <h1 v-on:mousemove="getConsole()">Home component</h1>
+        <p>Employee FirstName : {{ firstName }}</p>
+        <p>Employee LastName : {{ lastName }}</p>
+        <p>Email : {{ email }}</p>
+        <p>Mobile Number : {{ mobile }}</p>
+        <!-- print a full name  -->
+        <p>Full Name : {{ getFullName() }}</p>
+        <!-- print all data -->
+        <p>Get All Detail :{{ getDeta() }}</p>
+        <!-- print a only first name into data -->
+        <p>Name : {{ getDeta().firstName }}</p>
+        <p>Total Click : {{ count }}</p>
+        <button v-on:click="countClicks()">Click!!!</button>
+        <button v-on:dblclick="getMobilenumber()">Double Click</button>
+    </div>
+    <div id="rigth">
+        <h2>Second Div</h2>
+        <input type="number" v-model="count" />
+        <input type="text" v-model="name" />
+        <p>Your name is : {{ name }}</p>
+        <UserForm />
+    </div>
+    <div id="clearboth"></div>
+    <div>
+        <ListData />
+        <!-- set data and get into child component -->
+        <!-- Access parent component data into child component like:property , object ,function -->
+        <Child email="sarmand@zignuts.com" :users="users" :myFunction="myFunction"/>
+    </div>
 </template>
 
 <script>
-import UserForm from './UserForm.vue';
-import ListData from './ListData.vue'
+import UserForm from "./UserForm.vue";
+import ListData from "./ListData.vue";
+import Child from "./Child.vue";
 
 export default {
-    name: 'HomeView',
+    name: "HomeView",
     data() {
         return {
-            firstName: 'sarman',
-            lastName: 'Dasa',
-            email: 'dasa12@gmail.com',
-            mobile: '9078564534',
+            firstName: "sarman",
+            lastName: "Dasa",
+            email: "dasa12@gmail.com",
+            mobile: "9078564534",
             count: 0,
-            name:""
-        }
+            name: "",
+            users: [
+                {
+                    id:1,
+                    name: "sarman",
+                    email: "sarmand@zignuts.com",
+                    phone: "9767453445",
+                    gender: "Male",
+                },
+                {
+                    id:2,
+                    name: "dharmik",
+                    email: "dharmikd@zignuts.com",
+                    phone: "9767453444",
+                    gender: "Male",
+                },
+            ]
+        };
     },
     methods: {
         getFullName() {
@@ -51,28 +70,32 @@ export default {
         },
         getDeta() {
             return {
-                // send data into key-value pair 
-                firstName: 'vijay',
-                lastName: 'sharma',
-                email: 'vijay12@gmail.com',
-                mobile: '9078564533',
-            }
+                // send data into key-value pair
+                firstName: "vijay",
+                lastName: "sharma",
+                email: "vijay12@gmail.com",
+                mobile: "9078564533",
+            };
         },
         countClicks() {
             this.count += 1;
         },
         getMobilenumber() {
-            alert(this.mobile)
+            alert(this.mobile);
         },
         getConsole() {
-            console.log('mousemove event called')
+            console.log("mousemove event called");
+        },
+        myFunction() {
+            alert("call from child component");
         }
     },
     components: {
         UserForm,
         ListData,
-    }
-}
+        Child,
+    },
+};
 </script>
 
 <style scoped>
@@ -84,24 +107,26 @@ p {
     text-align: justify;
     font-weight: bold;
 }
-#left
-{
+
+#left {
     float: left;
     width: 45%;
     background-color: antiquewhite;
     padding: 10px;
 }
-#rigth
-{
+
+#rigth {
     float: right;
     width: 50%;
     text-align: left;
     background-color: aqua;
     padding: 10px;
 }
+
 h2 {
     color: #000;
 }
+
 #clearboth {
     clear: both;
 }
