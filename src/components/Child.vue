80ef5c8c106ecf15bf274a9800c312078d4b1b92
Sarman-Dasa <sarmand@zignuts.com>
2023-04-25 11:34:19 +0530

    define props to access parent data

---
+++ src/components/Child.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1> Hello for Child component</h1>
+    <p>Email From parent component : {{ email }}</p>
+   <ul>
+    <li v-for="user in users" :key="user.id">
+        {{ user.name }} || {{ user.email }} || {{ user.phone }} || {{ user.gender }}
+    </li>
+   </ul>
+   <button v-on:click="myFunction">Get Data</button>
+</template>
+
+<script>
+    export default {
+        name: 'ChildView',
+        props: {
+            email: String,
+            users:Object,
+            myFunction: Function,
+        }
+    }
+</script>
