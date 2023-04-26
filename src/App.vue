d8271272aed2775ad68cd16b83bb0190f993c7b4
Sarman-Dasa <sarmand@zignuts.com>
2023-04-26 17:34:22 +0530

    add router-link and router-view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
-  <Home />
+  <div class="link">
+    <router-link to="/">Home</router-link>
+    <router-link to="/php">Php</router-link>
+    <router-link to="/laravel">Laravel</router-link>
+    <router-link to="/vue">Vue js</router-link>
+    <router-link to="/user-data">user-data</router-link>
+    <router-link to="/watcher">Volume</router-link>
+  </div>
+<div class="main">
+  <router-view></router-view>
+</div>
+<teleport to="#footer">
+        <Footer />
+    </teleport>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/Home.vue'
+import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Home,
+    Footer,
   }
 }
 </script>
@@ -24,5 +37,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+}
+.link {
+  background-color: black;
+  padding: 20px;
+  width: 97%;
+}
+.link a {
+  color: #fff;
+  border: 2px solid rgb(30, 137, 208);
+  padding: 10px 20px;
+  margin: 10px;
+  text-decoration: none;
+}
+.main {
+  background-color:azure;
+    padding: 20px;
+    position: relative;
+    width: 50%;
+    margin: 2% 15%;
+    border: #000 5px solid;
 }
 </style>
