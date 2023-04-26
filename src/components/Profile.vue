19f5a526d6251a70c11251ad17fe97fc3d3e1fc7
Sarman-Dasa <sarmand@zignuts.com>
2023-04-26 18:31:57 +0530

    add profile component

---
+++ src/components/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>{{ profile }}' Profile page</h1>
+    <AlertBox>
+        Something bad happened.
+    </AlertBox>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+export default {
+    name: "UserProfile",
+    data() {
+        return {
+            profile: '',
+        }
+    },
+    mounted() {
+        const route = useRoute();
+        console.log("Route", route.params.id);
+        this.profile = route.params.id;
+    }
+}
+</script>
