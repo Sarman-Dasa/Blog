a5c360424c6f7c8a851621ba7cbb5faa349016a0
Sarman-Dasa <sarmand@zignuts.com>
2023-04-26 15:31:46 +0530

    add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <span>Footer</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quidem.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FooterView",
+    }
+</script>
+
+<style>
+.footer {
+    background-color: black;
+    color: #fff;
+    left: 0;
+    padding: 10px;
+    text-align: center;
+    font-size: larger;
+    width: 100%;
+    position: absolute;
+}
+</style>
