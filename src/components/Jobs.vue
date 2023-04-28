82a63af58c9c2f94e0831d4072e74dbf5b503819
Sarman-Dasa <sarmand@zignuts.com>
2023-04-28 18:01:44 +0530

    listing job name's using fetch method

---
+++ src/components/Jobs.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul>
+        <li v-for="job in jobs" :key="job.id">
+           <router-link :to="{name:'JobDetails',params:{id:job.id}}"> 
+            <span>{{ job.name }}</span>
+        </router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "JobsView",
+        data() {
+            return {
+                jobs: [],
+            }
+        },
+       async mounted() {
+            fetch("http://localhost:3000/jobs")
+            .then(response => response.json())
+            .then(data => this.jobs = data)
+            .catch(err => console.log(err.message));
+        }
+    }
+</script>
+
+<style>
+ul li {
+    display: inline-block;
+    margin: 0px 20px;
+    background-color: aqua;
+    padding: 10px 20px;
+    border-radius: 20px;
+    font-size: 18px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #000;
+    text-decoration: none;
+}
+a {
+    text-decoration: none !important;
+}
+li:hover {
+    background-color: brown;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
